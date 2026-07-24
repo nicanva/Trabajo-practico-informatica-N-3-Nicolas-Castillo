b8cff95ab8b52083f988721c86affd00679e934c
nicanva <nicocastillo.nsca@gmail.com>
2026-07-24 17:52:09 -0300

    Update bmi.c

--- bmi.c
+++ bmi.c
@@ -1,34 +1,39 @@
 #include <stdio.h>
-float peso;
-float altura;
-float resultado;
+// Link al repositorio: https://github.com/nicanva/Trabajo-practico-informatica-N-3-Nicolas-Castillo
+float peso, altura, resultado;
+
 int main(int argc, char *argv[]) {
-printf("Ingrese su peso en kg:\n");
-scanf("%f",&peso);
-printf("Ingrese su altura en metros\n");
-scanf("%f", &altura);
-resultado = peso / (altura * altura);
-printf("Su indice de masa corporal es: %.2f\n", resultado);
-printf("   Indice    |  Condicion\n");
-printf("-----------------------------\n");
-printf("   < 18.5    |  Bajo peso\n");
-printf(" 18.5 a 24.9 |  Normal\n");
-printf(" 25.0 a 29.9 |  Sobrepeso\n");
-printf("   >= 30     |  Obesidad\n");
-printf("-----------------------------\n");
-printf("Segun su indice, su condicion es: ");
-
-if (resultado < 18.5) 
-printf("Bajo peso\n");
-
-else if (resultado >= 18.5 && resultado <= 24.9) 
-printf("Normal\n");
-
-else if (resultado >= 25.0 && resultado <= 29.9) 
-printf("Sobrepeso\n");
-
-else 
-printf("Obesidad\n");
-	return 0;
+    printf("Calculadora de masa corporal (BMI)\n\n");
+    
+    do {
+        printf("Ingrese su peso en kg: ");
+        scanf("%f", &peso);
+        if (peso < 0) printf("Error: El peso no puede ser negativo. Intente de nuevo.\n");
+    } while (peso < 0);
+    
+    do {
+        printf("Ingrese su altura en metros: ");
+        scanf("%f", &altura);
+        if (altura < 0) printf("Error: La altura no puede ser negativa. Intente de nuevo.\n");
+    } while (altura < 0);
+    
+    resultado = peso / (altura * altura);
+    
+    printf("\nSu indice de masa corporal (BMI) es: %.2f\n\n", resultado);
+    
+    printf("   Indice    |  Condicion\n");
+    printf("-----------------------------\n");
+    printf("   < 18.5    |  Bajo peso\n");
+    printf(" 18.5 a 24.9 |  Normal\n");
+    printf(" 25.0 a 29.9 |  Sobrepeso\n");
+    printf("   >= 30     |  Obesidad\n");
+    printf("-----------------------------\n\n");
+    
+    printf("Segun su BMI, usted se encuentra en condicion de: ");
+    if (resultado < 18.5) printf("Bajo peso\n");
+    else if (resultado <= 24.9) printf("Normal\n");
+    else if (resultado <= 29.9) printf("Sobrepeso\n");
+    else printf("Obesidad\n");
+    
+    return 0;
 }
-
